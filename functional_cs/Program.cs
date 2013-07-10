4b57b1a9a986955b8b24d13ab5381037fe82185e
jasallen <github@deboever.us>
2013-07-10 00:31:19 -0400

    naive map reduce - but with Rx

--- functional_cs/Program.cs
+++ functional_cs/Program.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
 using System.Net.Http;
 using System.Web.Helpers;
 
@@ -20,21 +23,46 @@ namespace functional_cs
             // Set up Stream
             var http = new HttpClient();
             var url = "http://api.thriftdb.com/api.hnsearch.com/items/_search?start={0}&limit={1}&q=nsa";
-            var hits = GetHNhits(http, url, 0);
+            
+            //url = string.Format(url, 0, PageSize);
 
-            var map = hits.Select(_ => (int)_.item.points);
+            var s = Observable.Create<dynamic>(
+                (hitter) =>
+                {
+                    var cancel = new CancellationDisposable();
 
-            var reduction = map.Take(20).Aggregate(new{count=0,points=0}, (working , current) => new{count=working.count+1, points=(current - working.points) / (working.count+1) + working.points } );
+                    //var res = http.GetStringAsync(url).Result;
+                    //dynamic hn = Newtonsoft.Json.Linq.JObject.Parse(res);
+                    NewThreadScheduler.Default.Schedule(() =>
+                        {
+                            var hn = GetHNhits(http, url, 0);
 
-            Console.WriteLine(reduction);
+                            foreach (dynamic hit in hn)
+                            {
+                                if (cancel.Token.IsCancellationRequested) break;
+
+                                hitter.OnNext(hit);
+                            }
+                            
+                            hitter.OnCompleted();
+
+                        });
+
+                    return cancel;
+                })
+                .Take(20)
+                .Select(_ => (int)_.item.points)
+                .Aggregate(new { count = 0, points = 0 }, (working, current) => new { count = working.count + 1, points = (current - working.points) / (working.count + 1) + working.points })
+                .Subscribe(Console.WriteLine);
+            
+
+            //var hits = GetHNhits(http, url, 0);
+            //var map = hits.Select(_ => (int)_.item.points);
+            //var reduction = map.Take(20).Aggregate(new{count=0,points=0}, (working , current) => new{count=working.count+1, points=(current - working.points) / (working.count+1) + working.points } );
+            //Console.WriteLine(reduction);
+            
             Console.ReadLine();
-
-            //var titles = hits.Select(_ => _.item.title == null ? _.item.text : _.item.title);
-            //foreach (var title in titles)
-            //{
-            //    Console.WriteLine(title);
-            //}
-
+            Console.WriteLine("Cancelling...");            
         }
 
         private static IEnumerable<dynamic> GetHNhits(HttpClient http, string urltemplate, int nextResult)
@@ -44,9 +72,11 @@ namespace functional_cs
             dynamic hn = Newtonsoft.Json.Linq.JObject.Parse(res);
 
             foreach (dynamic hit in hn.results)
-            {
+            {                
                 yield return hit;
             }
+            
+            Thread.Sleep(2000);
 
             foreach (var subhit in GetHNhits(http, urltemplate, nextResult + PageSize))
             {
